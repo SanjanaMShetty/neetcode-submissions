e4d719215a5f92d80fa4f0bc738c43e6e86475c3
SanjanaMShetty <116268197+SanjanaMShetty@users.noreply.github.com>
2026-09-24 09:27:02 +0530

    Add: climbing-stairs - submission-2

---
+++ Data Structures & Algorithms/climbing-stairs/submission-2.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int dpHelper(int n,vector<int> &dp){
+        if(n==1 || n==2) return n;
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        return dp[n] = dpHelper(n-1,dp) + dpHelper(n-2,dp);
+    }
+    int climbStairs(int n) {
+        vector<int> dp(n+1,-1);
+        return dpHelper(n,dp);
+    }
+};
